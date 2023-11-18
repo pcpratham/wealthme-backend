4000693d6e6e54845da797c83b0809aa13dd3c16
Pratham Choudhary <prathamchoudhary924@gmail.com>
2023-11-18 18:38:06 +0530

    Final commit

--- config/database.js
+++ config/database.js
@@ -3,7 +3,13 @@ require("dotenv").config();
 
 
 const dbConnect = () => {
-    mongoose.connect(process.env.DATABASE_URL).then(() => { console.log("Database connection Done!"); }).catch((err) => { console.log("Error in database connection") });
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => { console.log("Database connection Done!"); }).catch((err) => {
+        console.log("Error in database connection");
+        console.log(err.message);
+    });
 }
 
 module.exports = dbConnect;
